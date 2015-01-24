7c353b09e2582d164c1af9930198b58817436a71
mintleaf <mintchipleaf@gmail.com>
2015-01-24 17:49:54 -0500

    Add Player script

---
+++ Unity Project/Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public static Player instance {get;private set;}
+
+	// Use this for initialization
+	void Awake () {
+		if(instance == null)
+			instance = this;
+	}
+}
