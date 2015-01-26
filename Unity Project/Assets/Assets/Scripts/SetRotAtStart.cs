a8152aabaa9e9a2f3dd09dfe94d7bb393120b816
Allen Brower <bslashingu@gmail.com>
2015-01-25 19:08:43 -0500

    no message

---
+++ Unity Project/Assets/Assets/Scripts/SetRotAtStart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetRotAtStart : MonoBehaviour {
+
+	public Vector2 rot = new Vector2 ();
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<vp_FPCamera> ().SetRotation (rot);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
