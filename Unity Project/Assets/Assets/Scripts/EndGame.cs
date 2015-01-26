b974b677bf4dce962de2ecd12b262a85e8a85513
mintleaf <mintchipleaf@gmail.com>
2015-01-25 19:04:41 -0500

    Add credits

---
+++ Unity Project/Assets/Assets/Scripts/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+	public GameObject credits;
+	public Camera creditsCamera;
+
+	// Use this for initialization
+	void Start () {
+		creditsCamera.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider entity){
+		//if(entity.name == "Player"){}
+		GameObject creditsCanvas = (GameObject)Instantiate (credits, Vector3.zero, Quaternion.identity);
+		creditsCamera.enabled = true;
+	}
+}
